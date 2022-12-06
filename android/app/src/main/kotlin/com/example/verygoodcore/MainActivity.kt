35353ce564f0a0b98685d458493c2cad11b16f29
Vlad Mestyashov <mestyashov.flutter@mail.ru>
2022-12-06 21:35:31 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.crm
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:crm/app/app.dart';
+import 'package:crm/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
