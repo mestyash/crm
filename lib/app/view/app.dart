f3ac059e00d9e730550c1ba397843556dd0590bc
Vlad Mestyashov <mestyashov.flutter@mail.ru>
2022-12-07 21:04:08 +0700

    configure app file

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,22 +1,25 @@
+import 'package:crm/core/project_theme.dart';
 import 'package:crm/counter/counter.dart';
 import 'package:crm/l10n/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(color: Color(0xFF13B9FF)),
-        colorScheme: ColorScheme.fromSwatch(
-          accentColor: const Color(0xFF13B9FF),
-        ),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) => MaterialApp(
+        theme: ProjectThemes.lightTheme,
+        themeMode: ThemeMode.light,
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        home: const CounterPage(),
       ),
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
     );
   }
 }
