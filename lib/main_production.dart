a8e2145297f5e300cff927e1615c1a5321e9947c
Vlad Mestyashov <mestyashov.flutter@mail.ru>
2022-12-15 21:00:53 +0700

    provide env file

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,8 +1,10 @@
 import 'package:crm/bootstrap.dart';
 import 'package:crm/core/locator.dart';
 import 'package:crm/features/common/app/view/app.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
   initGetIt();
   bootstrap(() => sl.get<App>());
 }
