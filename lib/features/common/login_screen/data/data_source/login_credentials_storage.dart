339462976048c02f647cbb30a129cd5368b00bfe
Vlad Mestyashov <mestyashov.flutter@mail.ru>
2022-12-15 21:51:45 +0700

    login credential storage

---
+++ lib/features/common/login_screen/data/data_source/login_credentials_storage.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:crm/core/data/data_source/secure_storage/secure_storage.dart';
+
+const LOGIN_CREDENTIALS = 'loginCredentials';
+
+class LoginStorage {
+  final SecureStorage storage;
+
+  LoginStorage({required this.storage});
+
+  Future<Map<String, String>?>? getCredentials() async {
+    final jsonCredential = await storage.read<String>(key: LOGIN_CREDENTIALS);
+    if (jsonCredential != null) {
+      return Map.castFrom(jsonDecode(jsonCredential) as Map<String, dynamic>);
+    }
+    return null;
+  }
+
+  Future<void> upsertCredentials(String login, String pass) async {
+    await storage.write<String>(
+      key: LOGIN_CREDENTIALS,
+      value: jsonEncode({
+        'login': login,
+        'pass': pass,
+      }),
+    );
+  }
+
+  Future<void> deleteCredentials() async {
+    await storage.delete(key: LOGIN_CREDENTIALS);
+  }
+}
