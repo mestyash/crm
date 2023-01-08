c708117a3c6835cdad36a034a00de419c07a89a0
Vlad Mestyashov <mestyashov.flutter@mail.ru>
2023-01-08 18:38:02 +0700

    custom app bar

---
+++ lib/core/presentation/ui/custom_app_bar/custom_app_bar.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+
+  const CustomAppBar({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(
+        title.toUpperCase(),
+      ),
+    );
+  }
+}
