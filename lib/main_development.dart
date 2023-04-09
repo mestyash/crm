7527d5be85f1615cdeaa850dfef786b54001d15a
Vlad Mestyashov <mestyashov.flutter@mail.ru>
2023-04-09 12:55:50 +0700

    main files fixes

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,10 +1,15 @@
 import 'package:crm/bootstrap.dart';
 import 'package:crm/core/locator.dart';
 import 'package:crm/features/common/app/view/app.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   await dotenv.load(fileName: '.env');
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL']!,
